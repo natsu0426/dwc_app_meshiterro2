27ffad7a5f11cfdc9d9fd6193b3734eee3a348b2
natsu0426 <natsu0426coffee@gmail.com>
2021-05-27 08:11:51 +0000

    6章まで追加

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'ae2bf5a6b86eb37dc4d8d44840355118a74a33bebda807146dc257d51c9a2e634d32171b0073af532d3ae8e45b26d966d1fdc9b30959c8402e4fef50f65691e2'
